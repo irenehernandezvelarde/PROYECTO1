a847bafaa6785287bc0b43b024fd5fc3a23ceab9
irenehernandezvelarde <ihernandezvelarde.cf@iesesteveterradas.cat>
2022-11-14 17:01:14 +0100

    // S1 P6 //A partir del Model de dades (MVC) crear un layout amb els controls distribuits en una zona per cada tipus de control.

--- Projecte1/src/View.java
+++ Projecte1/src/View.java
@@ -1,14 +1,21 @@
 import java.awt.EventQueue;
 
+import javax.swing.BoxLayout;
+import javax.swing.JComboBox;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JToggleButton;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JMenu;
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
@@ -31,9 +38,29 @@ public class View extends JFrame {
 			}
 		});
 	}
-	
 	public View() {
 		//CONFIG JFRAME
+		contentPane = new JPanel();
+		setContentPane(contentPane);
+		contentPane.setLayout(new BoxLayout(contentPane,BoxLayout.Y_AXIS));
+		
+		JPanel panelSwitches = new JPanel();
+		panelSwitches.setLayout(new BoxLayout(panelSwitches,BoxLayout.Y_AXIS));
+		
+		JPanel panelSliders = new JPanel();
+		panelSliders.setLayout(new BoxLayout(panelSliders,BoxLayout.Y_AXIS));
+		
+		JPanel panelComboBoxs = new JPanel();
+		panelComboBoxs.setLayout(new BoxLayout(panelComboBoxs,BoxLayout.Y_AXIS));
+        
+		JPanel panelLabels = new JPanel();
+		panelLabels.setLayout(new BoxLayout(panelLabels,BoxLayout.Y_AXIS));
+		
+        contentPane.add(panelSwitches);
+        contentPane.add(panelSliders);
+        contentPane.add(panelComboBoxs);
+        contentPane.add(panelLabels);
+        
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(0,0,500,500);
 		
@@ -44,6 +71,10 @@ public class View extends JFrame {
 		//Menu Arxiu
 		JMenu menuArxiu = new JMenu("Arxiu");
 		menuBar.add(menuArxiu);
+		
+		//Menu Visualitzacions
+		JMenu menuVisualitzacions = new JMenu("Visualitzaci\u00F3ns");
+		menuBar.add(menuVisualitzacions);
 		
 		JMenuItem itemCarregaConf = new JMenuItem("Carregar configuració");
 		itemCarregaConf.addActionListener(new ActionListener() {
@@ -62,25 +93,48 @@ public class View extends JFrame {
 				
 				//CARREGUEM DADES DEL FITXER
 				model.carregarConfiguracio();
+				ArrayList<ControlsBlock> controls = model.getControls();
 				
-				for (ArrayList a : model.getControls()) {
+				for(ArrayList a: controls) {
 					for (int b = 0; b<a.size(); b++) {
 						System.out.println(a.get(b).getClass());
+						switch(String.valueOf(a.get(b).getClass())) {
+						//
+						case "class CSwitch":
+							JToggleButton toggleButton = (JToggleButton) a.get(b);
+							panelSwitches.add(toggleButton);
+							contentPane.repaint();
+							break;
+						case "class CSlider":
+							CSlider n = (CSlider) a.get(b);
+							n.setPreferredSize(new Dimension(500, 50));
+							panelSliders.add(n);
+					        contentPane.repaint();
+							break;
+						case "class CDropdown":
+							JComboBox combo = (JComboBox) a.get(b);
+							combo.setPreferredSize(new Dimension(10, 10));
+							combo.setSize(getPreferredSize());
+							panelComboBoxs.add(combo);
+							panelLabels.repaint();
+					        contentPane.repaint();
+					        break;
+						case "class CSensor":
+							CSensor label = (CSensor) a.get(b);
+							label.setBackground(Color.pink);
+							label.setVisible(true);
+							panelLabels.add(label);
+							panelLabels.repaint();
+					        contentPane.repaint();
+					        break;
+						}
+							
 					}
 				}
-				//
-				
+				contentPane.revalidate();
 			}});
-		menuArxiu.add(itemCarregaConf);
 		
-		//Menu Visualitzacions
-		JMenu menuVisualitzacions = new JMenu("Visualitzaci\u00F3ns");
-		menuBar.add(menuVisualitzacions);
-		
-		//CONFIG JFRAME CONTENTPANE
-		contentPane = new JPanel();
-		setContentPane(contentPane);
-		contentPane.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		menuArxiu.add(itemCarregaConf);		
 	}
 
 }
